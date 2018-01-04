a75078a68c15104b4de2cf0a685c32566356194f
Ray Donnelly <mingw.android@gmail.com>
2018-01-04 21:33:12 +0000

    Removal of LDFLAGS_CC on macOS and modernization

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+libtoolize --copy
+autoreconf -vfi
+
 ./configure          \
   --prefix=${PREFIX} \
+  --enable-static    \
   --enable-shared    \
   --without-octave   \
   --without-matlab   \
   --without-guile
 
-make -j${CPU_COUNT}
-make install
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make install ${VERBOSE_AT}
