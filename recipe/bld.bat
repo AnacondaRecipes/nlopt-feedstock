cd22cddeb99eb4a1d5a9436770dde4a4e006c8c1
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-17 18:15:47 +0300

    Disable python on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,11 @@ mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 
+REM nlopt is used by r-nloptr so a Python interface isn't needed on win64
 cmake -LAH -G"NMake Makefiles"                     ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"        ^
+  -DNLOPT_PYTHON=OFF ^
   -DINSTALL_PYTHON_DIR="%SP_DIR%" ..
 if errorlevel 1 exit 1
 
