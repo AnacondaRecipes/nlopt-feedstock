01eba09b73bff94c8c82c5e9f9ff7871c923916c
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-17 12:35:14 +0300

    Disable LDFLAGS on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "$(uname)" = "Darwin" ]; then
-  export LDFLAGS="-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib ${LDFLAGS}"
-fi
+# if [ "$(uname)" = "Darwin" ]; then
+#   export LDFLAGS="-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib ${LDFLAGS}"
+# fi
 
 mkdir build && cd build || exit
 cmake \
