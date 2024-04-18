66d5020c3c9dc874260e667a59f3b71139881cc0
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-18 10:44:35 +0300

    Remove python stuff on unix, add more licenses from the src/algs folder

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,20 @@ mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 
-REM nlopt is used by r-nloptr so a Python interface isn't needed on win64
+REM nlopt is used by r-nloptr so a Python interface isn't needed on win64.
+REM Every supported programming language can have its interface of nlopt
+REM but creating the Python interface will require python in host and at runtime.
+REM That means then we need to build nlopt for all python versions (four at this moment).
+REM Hence, r-nloptr will be built for all 4 Python versions and the same applies
+REM to all 350 downstream R packages. It's a lot of redundancy.
+REM And R packages seem not to require the Python interface.
 cmake -LAH -G"NMake Makefiles"                     ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"        ^
   -DNLOPT_PYTHON=OFF ^
+  -DNLOPT_GUILE=OFF ^
+  -DNLOPT_MATLAB=OFF ^
+  -DNLOPT_OCTAVE=OFF ^
   ..
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
 
+set -euo pipeline
+
 if [ "$(uname)" = "Darwin" ]; then
   # osx uses a temp directory which messes up with cmake finding libraries.
   cd "${SRC_DIR}" || exit
 fi
 
 mkdir build && cd build || exit
+
+# nlopt is used by r-nloptr so a Python interface isn't needed on win64.
+# Every supported programming language can have its interface of nlopt
+# but creating the Python interface will require python in host and at runtime.
+# That means then we need to build nlopt for all python versions (four at this moment).
+# Hence, r-nloptr will be built for all 4 Python versions and the same applies
+# to all 350 downstream R packages. It's a lot of redundancy.
+# And R packages seem not to require the Python interface.
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DNLOPT_GUILE=OFF -DNLOPT_MATLAB=OFF -DNLOPT_OCTAVE=OFF \
+  -DNLOPT_PYTHON=OFF \
+  -DNLOPT_GUILE=OFF \
+  -DNLOPT_MATLAB=OFF \
+  -DNLOPT_OCTAVE=OFF \
   ..
 
 make install -j${CPU_COUNT}
 ctest --output-on-failure
-
-DIST_INFO_PATH=${SP_DIR}/${PKG_NAME}-${PKG_VERSION}.dist-info
-mkdir -p $DIST_INFO_PATH
-touch $DIST_INFO_PATH/METADATA
