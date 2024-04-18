a4e12e189a167e70399f0e9fe5f9cdb7d0d348f2
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2024-04-18 15:13:45 +0000

    nlopt 2.7.1 (#1)
    * nlopt 2.7.1
    
    * Add test commands
    
    * Disable python on win
    
    * Clean up python stuff
    
    * Remove python stuff on unix, add more licenses from the src/algs folder
    
    * Update recipe/build.sh
    
    Co-authored-by: Ian Fitchet <123584373+ifitchet@users.noreply.github.com>
    
    * Disable swig as we do not use python bindings
    
    ---------
    
    Co-authored-by: Ian Fitchet <123584373+ifitchet@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-libtoolize --copy
-autoreconf -vfi
+set -euo pipefail
 
-./configure          \
-  --prefix=${PREFIX} \
-  --enable-static    \
-  --enable-shared    \
-  --without-octave   \
-  --without-matlab   \
-  --without-guile
+if [ "$(uname)" = "Darwin" ]; then
+  # osx uses a temp directory which messes up with cmake finding libraries.
+  cd "${SRC_DIR}"
+fi
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
-make install ${VERBOSE_AT}
+mkdir build
+cd build
+
+# nlopt is used by r-nloptr so a Python interface isn't needed on win64.
+# Every supported programming language can have its interface of nlopt
+# but creating the Python interface will require python in host and at runtime.
+# That means then we need to build nlopt for all python versions (four at this moment).
+# Hence, r-nloptr will be built for all 4 Python versions and the same applies
+# to all 350 downstream R packages. It's a lot of redundancy.
+# And R packages seem not to require the Python interface.
+cmake \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DNLOPT_PYTHON=OFF \
+  -DNLOPT_SWIG=OFF \
+  -DNLOPT_GUILE=OFF \
+  -DNLOPT_MATLAB=OFF \
+  -DNLOPT_OCTAVE=OFF \
+  ..
+
+make install -j${CPU_COUNT}
+ctest --output-on-failure
